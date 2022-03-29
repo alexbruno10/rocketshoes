d7d879ff7afaf706bd6f035ff9adc246d73dd6de
Alex Bruno Neri Humberto <alexb3180_00@sicoobcredivar.com.br>
2022-03-29 16:42:38 -0300

    Coding hook useCart

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,18 +23,23 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      console.log('try')
+      const newCart = [...cart] //instancia um novo objeto para não alterar a imutabilidade
+
+      const productExist = newCart.find(product => product.id === productId) //verifica se existe o produto com o id passado
+
+
+
     } catch {
       console.log('catch')
     }
